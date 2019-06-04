e5d755d4e651ecace97aad8a62a5ef4f9578fbb7
Daniel Heath <daniel@heath.cc>
2019-06-05 09:01:50 +1000

    Fixup: skip validation if the password field is unset

--- lib/has_unpublished_password/never_leaked_to_hibp_validator.rb
+++ lib/has_unpublished_password/never_leaked_to_hibp_validator.rb
@@ -2,6 +2,7 @@
 
 class NeverLeakedToHibpValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    return unless value
     if HasUnpublishedPassword.has_been_published? value
       record.errors[attribute] << (options[:message] || "That's one of the first passwords a hacker would try.")
     end
